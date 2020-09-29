a73d21dc63e2d82ad827f6c537713cf46da93699
22mbaird <22mbaird@athenian.org>
2020-09-29 05:29:10 +0000

    Added methods and called in main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,45 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int x;
+        x = (int) (Math.random()*6)+1;
+        return x;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int x;
+        int y;
+        int z;
+        int[] x1 = new int[12];
+        for (int i=0; i < n; i++){
+            x= rollDie();
+            y = rollDie();
+            z= x+y;
+            x1[z-2]+=1;
+            
+        }
+        return x1;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Rolls: ");
+        String roll = scan.next(); 
+        int n = Integer.parseInt(roll);
+        int[] rb = sumOfTwoDice(n);
+        int w;
+
+        for (int i = 0; i<rb.length; i++){
+            w=i+2;
+            System.out.println(rb[i] + w);
+        }
+        scan.close();
+
+
+
     }
 }
